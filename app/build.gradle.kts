3382cae730c4c3b333e90d7ec17c8a47fae69d92
Emir Öztürk <emirzr56@gmail.com>
2023-09-24 15:29:55 +0300

    RecyclerViewAdapter added and finished

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.emirozturk.retrofit"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.emirozturk.retrofit"
---
+++ app/src/main/java/com/emirozturk/retrofit/adapter/RecyclerViewAdapter.kt
@@ -0,0 +1,41 @@
+package com.emirozturk.retrofit.adapter
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.emirozturk.retrofit.R
+import com.emirozturk.retrofit.model.CryptoModel
+
+class RecyclerViewAdapter(private val cryptoList: ArrayList<CryptoModel>, private val listener: Listener) : RecyclerView.Adapter<RecyclerViewAdapter.RowHolder>() {
+    private val colors: Array<String> = arrayOf("F8F9FA", "FFFFFF")
+
+    interface Listener {
+        fun onItemClick(cryptoModel: CryptoModel)
+    }
+
+    class RowHolder(view: View) : RecyclerView.ViewHolder(view) {
+        fun binding(cryptoModel: CryptoModel, colors: Array<String>, position: Int, listener: Listener) {
+            itemView.findViewById<TextView>(R.id.textName).text = cryptoModel.currency
+            itemView.findViewById<TextView>(R.id.textPrice).text = cryptoModel.price
+            itemView.setBackgroundColor(Color.parseColor(colors[position % 2]))
+            itemView.setOnClickListener {
+                listener.onItemClick(cryptoModel)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RowHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.row_layout, parent, false)
+        return RowHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return cryptoList.size
+    }
+
+    override fun onBindViewHolder(holder: RowHolder, position: Int) {
+        holder.binding(cryptoList[position], colors, position, listener)
+    }
+}
